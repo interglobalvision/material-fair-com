570c4434776ba0da786f6e69caaa63f412c6aa4e
Michael Ray-Von <mikey@interglobal.vision>
2016-06-22 17:49:46 -0500

    add event location taxonomy

--- lib/functions-custom.php
+++ lib/functions-custom.php
@@ -1,3 +1,22 @@
 <?php
 
 // Custom functions (like special queries, etc)
+
+function igv_taxonomies() {
+  register_taxonomy(
+    'event_location',
+    'event',
+    array(
+      'labels' => array(
+        'name' => 'Location',
+        'add_new_item' => 'Add New Location',
+        'new_item_name' => "New Event Location"
+      ),
+      'show_ui' => true,
+      'show_tagcloud' => false,
+      'hierarchical' => true
+    )
+  );
+}
+
+add_action( 'init', 'igv_taxonomies', 0 );
