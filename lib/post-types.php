7e8bc25edb42dc4b023de5346fae519187246800
Michael Ray-Von <mikey@interglobal.vision>
2016-06-22 17:42:52 -0500

    add event post type

--- lib/post-types.php
+++ lib/post-types.php
@@ -85,3 +85,46 @@ function register_cpt_press() {
 
     register_post_type( 'press', $args );
 }
+
+add_action( 'init', 'register_cpt_event' );
+
+function register_cpt_event() {
+
+    $labels = array( 
+        'name' => _x( 'Events', 'event' ),
+        'singular_name' => _x( 'Event', 'event' ),
+        'add_new' => _x( 'Add New', 'event' ),
+        'add_new_item' => _x( 'Add New Event', 'event' ),
+        'edit_item' => _x( 'Edit Event', 'event' ),
+        'new_item' => _x( 'New Event', 'event' ),
+        'view_item' => _x( 'View Event', 'event' ),
+        'search_items' => _x( 'Search Events', 'event' ),
+        'not_found' => _x( 'No events found', 'event' ),
+        'not_found_in_trash' => _x( 'No events found in Trash', 'event' ),
+        'parent_item_colon' => _x( 'Parent Event:', 'event' ),
+        'menu_name' => _x( 'Event', 'event' ),
+    );
+
+    $args = array( 
+        'labels' => $labels,
+        'hierarchical' => false,
+        
+        'supports' => array( 'title', 'editor', 'thumbnail' ),
+        
+        'public' => true,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'menu_position' => 5,
+        
+        'show_in_nav_menus' => true,
+        'publicly_queryable' => true,
+        'exclude_from_search' => false,
+        'has_archive' => true,
+        'query_var' => true,
+        'can_export' => true,
+        'rewrite' => true,
+        'capability_type' => 'post'
+    );
+
+    register_post_type( 'event', $args );
+}
