5a485629a85ae538c33fb93001047ae159201612
Michael Ray-Von <mikey@interglobal.vision>
2016-06-23 18:46:08 -0500

    rewrite IGV_Admin class and theme options functions for multiple theme options pages. add options pages

--- functions.php
+++ functions.php
@@ -50,7 +50,9 @@ register_nav_menus( array(
 get_template_part( 'lib/custom-gallery' );
 get_template_part( 'lib/post-types' );
 get_template_part( 'lib/meta-boxes' );
+get_template_part( 'lib/theme-options-class' );
 get_template_part( 'lib/theme-options' );
+
 
 
 // Add third party PHP libs
--- lib/theme-options.php
+++ lib/theme-options.php
@@ -1,248 +1,157 @@
 <?php
+$prefix = '_igv_';
 
-/**
- * CMB2 Theme Options
- * @version 0.1.0
- */
-class IGV_Admin {
+// SITE OPTIONS
 
-  /**
-    * Option key, and option page slug
-    * @var string
-    */
-  private $key = 'IGV_options';
-
-  /**
-    * Option prefix
-    * @var string
-    */
-  private $prefix = '_igv_';
-
-  /**
-    * Options page metabox id
-    * @var string
-    */
-  private $metabox_id = 'IGV_option_metabox';
-
-  /**
-   * Options Page title
-   * @var string
-   */
-  protected $title = 'Site Options';
-
-  /**
-   * Options Page hook
-   * @var string
-   */
-  protected $options_page = '';
-
-  /**
-   * Constructor
-   * @since 0.1.0
-   */
-  public function __construct() {
-    // Set our title
-    $this->title = __( 'Site Options', 'IGV' );
-  }
-
-  /**
-   * Initiate our hooks
-   * @since 0.1.0
-   */
-  public function hooks() {
-    add_action( 'admin_init', array( $this, 'init' ) );
-    add_action( 'admin_menu', array( $this, 'add_options_page' ) );
-    add_action( 'cmb2_init', array( $this, 'add_options_page_metabox' ) );
-  }
-
-
-  /**
-   * Register our setting to WP
-   * @since  0.1.0
-   */
-  public function init() {
-    register_setting( $this->key, $this->key );
-  }
-
-  /**
-   * Add menu options page
-   * @since 0.1.0
-   */
-  public function add_options_page() {
-    $this->options_page = add_menu_page( $this->title, $this->title, 'manage_options', $this->key, array( $this, 'admin_page_display' ) );
-  }
-
-  /**
-   * Admin page markup. Mostly handled by CMB2
-   * @since  0.1.0
-   */
-  public function admin_page_display() {
-    ?>
-    <div class="wrap cmb2-options-page <?php echo $this->key; ?>">
-      <h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
-      <?php cmb2_metabox_form( $this->metabox_id, $this->key ); ?>
-    </div>
-    <?php
-  }
-
-  /**
-   * Add the options metabox to the array of metaboxes
-   * @since  0.1.0
-   */
-  function add_options_page_metabox() {
-
-    $options_metabox = new_cmb2_box( array(
-      'id'      => $this->metabox_id,
-      'hookup'  => false,
-      'show_on' => array(
-        // These are important, don't remove
-        'key'   => 'options-page',
-        'value' => array( $this->key, )
-      ),
-    ) );
-
-    $options_metabox->add_field( array(
+$page_key = $prefix . 'theme_options';
+$page_id = $prefix . 'theme_options_page';
+$page_title = 'Site Options';
+$metabox = array(
+  'id'         => $page_id, //id used as tab page slug, must be unique
+  'title'      => $page_title,
+  'show_on'    => array( 'key' => 'options-page', 'value' => array( $page_id ), ), //value must be same as id
+  'show_names' => true,
+  'fields'     => array(
+    array(
       'name' => __( 'Fair Venue Name', 'cmb2' ),
       'desc' => __( '', 'cmb2' ),
       'id'   => $prefix . 'venue_name',
       'type' => 'text',
-    ) );
-
-    $options_metabox->add_field( array(
+    ),
+    array(
       'name' => __( 'Fair Start Date', 'cmb2' ),
       'desc' => __( '', 'cmb2' ),
       'id'   => $prefix . 'fair_start',
       'type' => 'text_date_timestamp',
-    ) );
-
-    $options_metabox->add_field( array(
+    ),
+    array(
       'name' => __( 'Fair End Date', 'cmb2' ),
       'desc' => __( '', 'cmb2' ),
       'id'   => $prefix . 'fair_end',
       'type' => 'text_date_timestamp',
-    ) );
-
-    $options_metabox->add_field( array(
-      'name' => __( 'Primary Sponsor Logo', 'IGV' ),
-      'desc' => __( 'displayed in header', 'IGV' ),
-      'id'   => $this->prefix . 'primary_sponsor_logo',
-      'type' => 'file',
-    ) );
-
-    $options_metabox->add_field( array(
-      'name' => __( 'Primary Sponsor URL', 'cmb2' ),
-      'desc' => __( '', 'cmb2' ),
-      'id'   => $prefix . 'primary_sponsor_url',
-      'type' => 'text_url',
-    ) );
-
-    $options_metabox->add_field( array(
+    ),
+    array(
       'name' => __( 'Show VIP Login', 'cmb2' ),
       'desc' => __( '', 'cmb2' ),
       'id'   => $prefix . 'show_vip_login',
       'type' => 'checkbox',
-    ) );
-
-    $options_metabox->add_field( array(
+    ),
+    array(
       'name' => __( 'App Login Button Text', 'cmb2' ),
       'desc' => __( '', 'cmb2' ),
       'id'   => $prefix . 'app_login_text',
       'type' => 'text',
-    ) );
-
-    $options_metabox->add_field( array(
+    ),
+    array(
       'name' => __( 'Publish Exhibitors', 'cmb2' ),
       'desc' => __( '', 'cmb2' ),
       'id'   => $prefix . 'publish_exhibitors',
       'type' => 'checkbox',
-    ) );
-
-    $options_metabox->add_field( array(
+    ),
+    array(
       'name' => __( 'Publish Program', 'cmb2' ),
       'desc' => __( '', 'cmb2' ),
       'id'   => $prefix . 'publish_program',
       'type' => 'checkbox',
-    ) );
-
-    $options_metabox->add_field( array(
+    ),
+    array(
       'name' => __( 'Show Reading Material', 'cmb2' ),
       'desc' => __( '', 'cmb2' ),
       'id'   => $prefix . 'show_reading_material',
       'type' => 'checkbox',
-    ) );
-
-    // SOCIAL MEDIA OPTIONS
-
-    $options_metabox->add_field( array(
-      'name' => __( 'Social Media', 'cmb2' ),
-      'desc' => __( 'urls and accounts for different social media platforms. For use in menus and metadata', 'cmb2' ),
-      'id'   => $this->prefix . 'socialmedia_title',
-      'type' => 'title',
-    ) );
-
-    $options_metabox->add_field( array(
-      'name' => __( 'Facebook Page URL', 'IGV' ),
-      'desc' => __( '', 'IGV' ),
-      'id'   => $this->prefix . 'socialmedia_facebook_url',
-      'type' => 'text',
-    ) );
-
-    $options_metabox->add_field( array(
-      'name' => __( 'Twitter Account Handle', 'IGV' ),
-      'desc' => __( '', 'IGV' ),
-      'id'   => $this->prefix . 'socialmedia_twitter',
-      'type' => 'text',
-    ) );
-
-    $options_metabox->add_field( array(
-      'name' => __( 'Instagram Account Handle', 'IGV' ),
-      'desc' => __( '', 'IGV' ),
-      'id'   => $this->prefix . 'socialmedia_instagram',
-      'type' => 'text',
-    ) );
+    ),
 
     // METADATA OPTIONS
 
-    $options_metabox->add_field( array(
+    array(
       'name' => __( 'Metadata options', 'cmb2' ),
       'desc' => __( 'Settings relating to open graph, facebook and twitter sharing, and other social media metadata', 'cmb2' ),
-      'id'   => $this->prefix . 'og_title',
+      'id'   => $prefix . 'og_title',
       'type' => 'title',
-    ) );
-
-    $options_metabox->add_field( array(
+    ),
+    array(
       'name' => __( 'Open Graph image', 'IGV' ),
       'desc' => __( 'primarily displayed on Facebook, but other locations as well', 'IGV' ),
-      'id'   => $this->prefix . 'og_image',
+      'id'   => $prefix . 'og_image',
       'type' => 'file',
-    ) );
-
-    $options_metabox->add_field( array(
+    ),
+    array(
       'name' => __( 'Logo', 'IGV' ),
       'desc' => __( '(options) ', 'IGV' ),
-      'id'   => $this->prefix . 'metadata_logo',
+      'id'   => $prefix . 'metadata_logo',
       'type' => 'file',
-    ) );
-
-    $options_metabox->add_field( array(
+    ),
+    array(
       'name' => __( 'Facebook App ID', 'IGV' ),
       'desc' => __( '(optional)', 'IGV' ),
-      'id'   => $this->prefix . 'og_fb_app_id',
+      'id'   => $prefix . 'og_fb_app_id',
       'type' => 'text',
-    ) );
+    )
+  )
+);
+
+IGV_init_options_page($page_id, $page_id, $page_title, $metabox);
 
 
-  // PARTNERS
+// SPONSORS
 
-    $options_metabox->add_field( array(
-      'name' => __( 'Partners', 'cmb2' ),
+$page_key = $prefix. 'sponsors_options';
+$page_id = $prefix . 'sponsors_options_page';
+$page_title = 'Sponsors';
+$metabox = array(
+  'id'         => $page_id, //id used as tab page slug, must be unique
+  'title'      => $page_title,
+  'show_on'    => array( 'key' => 'options-page', 'value' => array( $page_id ), ), //value must be same as id
+  'show_names' => true,
+  'fields'     => array(
+    array(
+      'name' => __( 'Primary Sponsor Logo', 'IGV' ),
+      'desc' => __( 'displayed in header', 'IGV' ),
+      'id'   => $prefix . 'primary_sponsor_logo',
+      'type' => 'file',
+    ),
+    array(
+      'name' => __( 'Primary Sponsor URL', 'cmb2' ),
       'desc' => __( '', 'cmb2' ),
-      'id'   => $this->prefix . 'partners_title',
+      'id'   => $prefix . 'primary_sponsor_url',
+      'type' => 'text_url',
+    ),
+    array(
+      'name' => __( 'Lead Sponsors', 'cmb2' ),
+      'id'   => $prefix . 'sponsors_title',
       'type' => 'title',
-    ) );
-
-    $partners_group = $options_metabox->add_field( array(
-      'id'          => $this->prefix . 'partners',
+    ),
+    array(
+      'id'          => $prefix . 'sponsors_group',
+      'type'        => 'group',
+      'description' => __( '', 'cmb2' ),
+      'options'     => array(
+        'group_title'   => __( 'Lead Sponsor {#}', 'cmb2' ), // {#} gets replaced by row number
+        'add_button'    => __( 'Add Another Lead Sponsor', 'cmb2' ),
+        'remove_button' => __( 'Remove Lead Sponsor', 'cmb2' ),
+        'sortable'      => true, // beta
+        // 'closed'     => true, // true to have the groups closed by default
+      ),
+      'fields'      => array(
+        array(
+          'name'       => __( 'Logo', 'cmb2' ),
+          'id'         => 'logo',
+          'type'       => 'file',
+        ),
+        array(
+          'name'       => __( 'URL', 'cmb2' ),
+          'id'         => 'url',
+          'type'       => 'text_url',
+        )
+      )
+    ), 
+    array(
+      'name' => __( 'Partners', 'cmb2' ),
+      'id'   => $prefix . 'partners_title',
+      'type' => 'title',
+    ),
+    array(
+      'id'          => $prefix . 'partners_group',
       'type'        => 'group',
       'description' => __( '', 'cmb2' ),
       'options'     => array(
@@ -252,63 +161,56 @@ class IGV_Admin {
         'sortable'      => true, // beta
         // 'closed'     => true, // true to have the groups closed by default
       ),
-    ) );
-
-    $options_metabox->add_group_field( $partners_group, array(
-      'name'       => __( 'URL', 'cmb2' ),
-      'id'         => 'url',
-      'type'       => 'text_url',
-    ) );
-
-    $options_metabox->add_group_field( $partners_group, array(
-      'name' => __( 'Logo', 'cmb2' ),
-      'id'   => 'logo',
-      'type' => 'file',
-    ) );
-
-  }
-
-  /**
-   * Public getter method for retrieving protected/private variables
-   * @since  0.1.0
-   * @param  string  $field Field to retrieve
-   * @return mixed          Field value or exception is thrown
-   */
-  public function __get( $field ) {
-    // Allowed fields to retrieve
-    if ( in_array( $field, array( 'key', 'metabox_id', 'title', 'options_page' ), true ) ) {
-      return $this->{$field};
-    }
-
-    throw new Exception( 'Invalid property: ' . $field );
-  }
-
-}
-
-/**
- * Helper function to get/return the IGV_Admin object
- * @since  0.1.0
- * @return IGV_Admin object
- */
-function IGV_admin() {
-  static $object = null;
-  if ( is_null( $object ) ) {
-    $object = new IGV_Admin();
-    $object->hooks();
-  }
-
-  return $object;
-}
-
-/**
- * Wrapper function around cmb2_get_option
- * @since  0.1.0
- * @param  string  $key Options array key
- * @return mixed        Option value
- */
-function IGV_get_option( $key = '' ) {
-  return cmb2_get_option( IGV_admin()->key, $key );
-}
+      'fields'      => array(
+        array(
+          'name'       => __( 'Logo', 'cmb2' ),
+          'id'         => 'logo',
+          'type'       => 'file',
+        ),
+        array(
+          'name'       => __( 'URL', 'cmb2' ),
+          'id'         => 'url',
+          'type'       => 'text_url',
+        )
+      )
+    )    
+  )
+);
 
 // Get it started
-IGV_admin();
+IGV_init_options_page($page_key, $page_id, $page_title, $metabox);
+
+
+// SOCIAL MEDIA 
+
+$page_key = $prefix . 'social_options';
+$page_id = $prefix . 'social_options_page';
+$page_title = 'Social Media';
+$metabox = array(
+  'id'         => $page_id, //id used as tab page slug, must be unique
+  'title'      => $page_title,
+  'show_on'    => array( 'key' => 'options-page', 'value' => array( $page_id ), ), //value must be same as id
+  'show_names' => true,
+  'fields'     => array(
+    array(
+      'name' => __( 'Facebook Page URL', 'IGV' ),
+      'desc' => __( '', 'IGV' ),
+      'id'   => $prefix . 'socialmedia_facebook_url',
+      'type' => 'text',
+    ),
+    array(
+      'name' => __( 'Twitter Account Handle', 'IGV' ),
+      'desc' => __( '', 'IGV' ),
+      'id'   => $prefix . 'socialmedia_twitter',
+      'type' => 'text',
+    ),
+    array(
+      'name' => __( 'Instagram Account Handle', 'IGV' ),
+      'desc' => __( '', 'IGV' ),
+      'id'   => $prefix . 'socialmedia_instagram',
+      'type' => 'text',
+    )
+  )
+);
+
+IGV_init_options_page($page_id, $page_id, $page_title, $metabox);
