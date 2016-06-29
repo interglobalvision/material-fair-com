bbe5b749b9544db614b5ec54abe3e9435a3292f0
Michael Ray-Von <mikey@interglobal.vision>
2016-06-29 18:54:32 -0500

    add sponsor logo image size

--- lib/thumbnail-sizes.php
+++ lib/thumbnail-sizes.php
@@ -7,6 +7,7 @@ if( function_exists( 'add_theme_support' ) ) {
 if( function_exists( 'add_image_size' ) ) {
   add_image_size( 'admin-thumb', 150, 150, false );
   add_image_size( 'opengraph', 1200, 630, true );
+  add_image_size( 'sponsor', 160, 80, false );
 
   add_image_size( 'name', 199, 299, true );
 }
