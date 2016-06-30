f0fc3094c1fe1831fd528381c519bf2380129d9a
Michael Ray-Von <mikey@interglobal.vision>
2016-06-30 14:39:24 -0500

    add intro section

--- front-page.php
+++ front-page.php
@@ -3,6 +3,11 @@ get_header();
 
 $splash_id = IGV_get_option('_igv_site_options', '_igv_front_page_splash_id');
 $splash_link = IGV_get_option('_igv_site_options', '_igv_front_page_splash_url');
+
+$headline_en = IGV_get_option('_igv_site_options', '_igv_front_headline_en');
+$headline_es = IGV_get_option('_igv_site_options', '_igv_front_headline_es');
+$intro_en = IGV_get_option('_igv_site_options', '_igv_front_description_en');
+$intro_es = IGV_get_option('_igv_site_options', '_igv_front_description_es');
 ?>
 
 <!-- main content -->
@@ -22,7 +27,14 @@ $splash_link = IGV_get_option('_igv_site_options', '_igv_front_page_splash_url')
   <section id="front-intro" class="section">
     <div class="container">
       <div class="row">
-
+        <div class="col col-l col-l-4">
+          <?php _e(!empty($headline_en) && !empty($headline_es) ? '[:en]' . $headline_en . '[:es]' . $headline_es : ''); ?>
+        </div>
+        <div class="col col-l col-l-8">
+          <div class="text-columns-2">
+            <?php _e(!empty($intro_en) && !empty($intro_es) ? '[:en]' . $intro_en . '[:es]' . $intro_es : ''); ?>
+          </div>
+        </div>
       </div>
     </div>
   </section>
