c0f256aa57aec2668ea16bb8e05f9a7bae6e6e3d
Patrick Best <patrickbest@patrickbest.com>
2016-06-30 18:51:45 +0100

    move lib functions

--- lib/functions-filters.php
+++ lib/functions-filters.php
@@ -11,3 +11,23 @@ function add_slug_body_class( $classes ) {
   return $classes;
 }
 add_filter( 'body_class', 'add_slug_body_class' );
+
+// Show exhibitor year column
+add_filter( 'manage_exhibitor_posts_columns', 'add_exhibitor_year_column' );
+function add_exhibitor_year_column($columns) {
+    $columns['exhibitor_year'] = __( 'Year', 'IGV' );
+    return $columns;
+}
+
+add_action( 'manage_exhibitor_posts_custom_column' , 'display_exhibitor_year_column', 10, 2 );
+function display_exhibitor_year_column( $column, $post_id ) {
+  switch ( $column ) {
+    case 'exhibitor_year' :
+    $year = get_post_meta( $post_id , '_igv_exhibitor_year' , false );
+    if ( !empty($year[0]) )
+        echo $year[0];
+    else
+        _e( 'Not supported in theme', 'IGV' );
+    break;
+  }
+}
--- lib/functions-misc.php
+++ lib/functions-misc.php
@@ -48,26 +48,6 @@ function clean_site_blog_info() {
 }
 add_action( 'after_setup_theme', 'clean_site_blog_info' );
 
-
-// Show exhibitor year column
-add_filter( 'manage_exhibitor_posts_columns', 'add_exhibitor_year_column' );
-add_action( 'manage_exhibitor_posts_custom_column' , 'display_exhibitor_year_column', 10, 2 );
-function add_exhibitor_year_column($columns) {
-    $columns['exhibitor_year'] = __( 'Year', 'IGV' );
-    return $columns;
-}
-function display_exhibitor_year_column( $column, $post_id ) {
-  switch ( $column ) {
-    case 'exhibitor_year' :
-    $year = get_post_meta( $post_id , '_igv_exhibitor_year' , false );
-    if ( !empty($year[0]) )
-        echo $year[0];
-    else
-        _e( 'Not supported in theme', 'IGV' );
-    break;
-  }
-}
-
 // custom login logo
 /*
 function custom_login_logo() {
