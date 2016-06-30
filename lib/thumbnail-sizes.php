728d43a043516080d2b427fa02af0c0504c79eb4
Michael Ray-Von <mikey@interglobal.vision>
2016-06-30 14:24:32 -0500

    add splash image size

--- lib/thumbnail-sizes.php
+++ lib/thumbnail-sizes.php
@@ -8,6 +8,7 @@ if( function_exists( 'add_image_size' ) ) {
   add_image_size( 'admin-thumb', 150, 150, false );
   add_image_size( 'opengraph', 1200, 630, true );
   add_image_size( 'sponsor', 300, 100, false );
+  add_image_size( 'splash', 1440, 550, true );
 
   add_image_size( 'name', 199, 299, true );
 }
