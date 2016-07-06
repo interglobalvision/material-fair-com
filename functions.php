a5ff6e763cd6ee4e16de574b52e9521ae87ee20e
Michael Ray-Von <mikey@interglobal.vision>
2016-07-06 11:16:48 -0500

    get taxonomy lib

--- functions.php
+++ functions.php
@@ -49,6 +49,7 @@ register_nav_menus( array(
 
 get_template_part( 'lib/custom-gallery' );
 get_template_part( 'lib/post-types' );
+get_template_part( 'lib/taxonomies' );
 get_template_part( 'lib/meta-boxes' );
 get_template_part( 'lib/theme-options/theme-options' );
 
