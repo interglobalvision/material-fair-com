0224865205310ac7c6f43450afa2d190dfca50f5
Michael Ray-Von <mikey@interglobal.vision>
2016-07-06 21:57:41 -0500

    add press query filter

--- lib/functions-filters.php
+++ lib/functions-filters.php
@@ -45,3 +45,64 @@ function exclude_past_exhibitors( $query ) {
   }
 }
 add_action( 'pre_get_posts', 'exclude_past_exhibitors' );
+
+
+// Only get current year exhibitors with thumbnail for archive
+function filter_press_posts( $query ) {
+  if ( $query->is_main_query() && is_post_type_archive('press') && !is_admin() ) {
+
+    $current_year_id = IGV_get_option('_igv_site_options', '_igv_current_fair_year');
+
+
+    //add metaquery for highlight posts
+    $highlight_metaquery = array( 
+      array(
+        'key' => '_igv_press_highlight',
+        'value' => 'on',
+      ),
+    );
+    $args = array(
+      'post_type' => 'press',
+      'meta_query' => $highlight_metaquery,
+      'numberposts' => 2,
+    );
+
+    //get highlight posts
+    $highlight_posts = get_posts($args);
+
+    //put hightlight post ids in array
+    $highlight_ids = array();
+    foreach ( $highlight_posts as $post ) {
+       $highlight_ids[] += $post->ID;
+    }
+
+    //exclude highlighted posts from query
+    $query->set( 'post__not_in', $highlight_ids );
+
+
+
+    //posts with current year
+    $taxquery = array(
+      array(
+        'taxonomy' => 'fair_year',
+        'field' => 'term_id',
+        'terms' => $current_year_id,
+      ),
+    );
+    //get non-highlight, current year posts
+    $args = array(
+      'post_type' => 'press',
+      'tax_query' => $taxquery,
+      'post__not_in' => $highlight_ids
+    );
+    $current_year_posts = get_posts($args);
+
+
+    //if there are more than 2 current posts
+    //exclude all others from query
+    if (count($current_year_posts) > 2) {
+      $query->set( 'tax_query', $taxquery );
+    }
+  }
+}
+add_action( 'pre_get_posts', 'filter_press_posts' );
