2d3f36629dee49bcfe978504e7b699d46051de6c
Michael Ray-Von <mikey@interglobal.vision>
2016-07-11 16:59:47 -0500

    add target _blank. put font-size-h4 class on parent container

--- partials/single-exhibitor/basic-info.php
+++ partials/single-exhibitor/basic-info.php
@@ -25,18 +25,20 @@ if (!empty($stand)) {
       </div>
       <div class="col col-l col-l-6">
         <div class="font-size-h4">
-          <?php the_content(); ?>
+          <?php 
+            the_content(); 
+
+            if (!empty($website)) {
+              echo '<p><a target="_blank" href="' . esc_url($website) . '">' . $website . '</a></p>';
+            }
+
+            if (!empty($address)) {
+              echo '<p>' . apply_filters( 'the_content', $address ) . '</p>';
+            }
+          ?>
         </div>
 
-        <?php 
-          if (!empty($website)) {
-            echo '<p class="font-size-h4"><a href="' . esc_url($website) . '">' . $website . '</a></p>';
-          }
-
-          if (!empty($address)) {
-            echo '<div class="font-size-h4">' . apply_filters( 'the_content', $address ) . '</div>';
-          }
-
+        <?php
           if (!empty($staff)) {
         ?>
         <ul>
@@ -44,7 +46,7 @@ if (!empty($stand)) {
           foreach ($staff as $member) {
         ?>
           <li class="margin-bottom-micro">
-            <div class="font-size-h4"><?php echo $member['name']; ?></div>
+            <h4><?php echo $member['name']; ?></h4>
             <?php echo $member['role']; ?>
           </li>
         <?php
