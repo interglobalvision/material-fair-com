f754f163c6596d9fd980565ee5c8eab7f93f837b
Michael Ray-Von <mikey@interglobal.vision>
2016-07-11 16:53:17 -0500

    add archive-event template

---
+++ archive-event.php
@@ -0,0 +1,19 @@
+<?php
+get_header();
+?>
+
+<!-- main content -->
+
+<main id="main-content">
+
+<?php get_template_part('partials/archive-event/introduction'); ?>
+
+<?php get_template_part('partials/archive-event/highlights'); ?>
+
+<!-- end main-content -->
+
+</main>
+
+<?php
+get_footer();
+?>
