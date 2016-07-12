19799ef885c0f00976ec02eaf330b5ce4f432870
Michael Ray-Von <mikey@interglobal.vision>
2016-07-12 18:25:49 -0500

    add partners page template. move partners section into /partials. modify parterns section for use on partners page

---
+++ page-partners.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Template Name: Partners
+ *
+ */
+
+get_header();
+
+
+
+?>
+
+<!-- main content -->
+
+<main id="main-content">
+
+  <?php get_template_part('partials/partners'); ?>
+
+</main>
+
+<?php
+get_footer();
+?>
