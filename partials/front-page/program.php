5dfbc2bcb6d0694112b3ac4a5dfac0dfa9d266c2
Michael Ray-Von <mikey@interglobal.vision>
2016-07-12 15:21:53 -0500

    fix front-page program section meta ids. use archive-event introduction partial

--- partials/front-page/program.php
+++ partials/front-page/program.php
@@ -1,46 +1,16 @@
 <?php
 $publish_program = IGV_get_option('_igv_page_options', '_igv_publish_program');
 
-$program_image_id = IGV_get_option('_igv_page_options', '_igv_program_image_id');
-$program_text = IGV_get_option('_igv_page_options', '_igv_program_temp_text');
+$program_image_id = IGV_get_option('_igv_page_options', '_igv_program_page_image_id');
+$program_text = IGV_get_option('_igv_page_options', '_igv_program_page_text');
 
 if ( $publish_program == 'on' ) {
 
   get_template_part('partials/archive-event/highlights');
 
-} else {
-?>
-  <section id="front-program" class="section">
-    <div class="container">
-      <div class="row">
-        <div class="col col-l col-l-12">
-          <h2 class="text-align-left"><?php _e('[:en]Program[:es]Programa'); ?></h2>
-        </div>
-      </div>
-<?php 
-        if (!empty($program_image_id) || !empty($program_text)) { 
-?>
-      <div class="row">
-<?php 
-          if (!empty($program_image_id)) { 
-?>
-        <div class="col col-l col-l-6">
-          <?php echo wp_get_attachment_image($program_image_id, 'col-6'); ?>
-        </div>
-<?php 
-          }
-          if (!empty($program_text)) {
-?>
-        <div class="col col-l col-l-6">
-          <?php echo apply_filters( 'the_content', $program_text ); ?>
-        </div>
-<?php 
-          }
-        }
-?>
-      </div>
-    </div>
-  </section>
-<?php 
+} elseif (!empty($program_image_id) || !empty($program_text)) {
+
+  get_template_part('partials/archive-event/introduction');
+
 }
 ?>
