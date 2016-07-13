4b9b6f66460e435959b47d77b8cf7abf2fa21ee0
Michael Ray-Von <mikey@interglobal.vision>
2016-07-13 16:43:27 -0500

    add dudes. make dem dance

--- 404.php
+++ 404.php
@@ -5,12 +5,23 @@ get_header();
 <!-- main content -->
 
 <main id="main-content">
-  <section>
+  <section id="404">
     <div class="container">
       <div class="row">
         <div class="col col-l col-l-12 text-align-center">
           <h1>404</h1>
-          <h2><?php _e('[:en]Are you lost?[:es]¿Estás perdido?'); ?></h2>
+          <div class="font-size-h2 font-sans"><?php _e('[:en]Are you lost?[:es]¿Estás perdido?'); ?></div>
+        </div>
+      </div>
+      <div class="row">
+        <div  class="col col-l col-l-4 text-align-center">
+          <img class="dude" src="<?php bloginfo('stylesheet_directory'); ?>/img/src/dude_1.svg">
+        </div>
+        <div  class="col col-l col-l-4 text-align-center">
+          <img class="dude" src="<?php bloginfo('stylesheet_directory'); ?>/img/src/dude_2.svg">
+        </div>
+        <div  class="col col-l col-l-4 text-align-center">
+          <img class="dude" src="<?php bloginfo('stylesheet_directory'); ?>/img/src/dude_3.svg">
         </div>
       </div>
     </div>
