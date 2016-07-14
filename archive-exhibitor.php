48557697d80965ca8df8bfe8f7f5c9f41eb21436
Michael Ray-Von <mikey@interglobal.vision>
2016-07-14 11:18:18 -0500

    update conditional for showing exhibitors on exhibitors archive

--- archive-exhibitor.php
+++ archive-exhibitor.php
@@ -5,6 +5,7 @@ $publish_exhibitors = IGV_get_option('_igv_page_options', '_igv_publish_exhibito
 $exhibitors_apply_text = IGV_get_option('_igv_page_options', '_igv_exhibitors_apply_text');
 $apply_url = IGV_get_option('_igv_site_options', '_igv_apply_url');
 $apply_end = IGV_get_option('_igv_site_options', '_igv_apply_end');
+$current_year_id = IGV_get_option('_igv_site_options', '_igv_current_fair_year');
 ?>
 
 <!-- main content -->
@@ -19,7 +20,7 @@ $apply_end = IGV_get_option('_igv_site_options', '_igv_apply_end');
 // if the application hasn't ended and 
 // exhibitors are not published, 
 // show the Apply Now section
-if ($publish_exhibitors == 'on') { 
+if (($current_year_id == get_fair_year_id() && $publish_exhibitors == 'on') || $current_year_id != $fair_year_id) {
 
   get_template_part('partials/archive-exhibitor/exhibitors');
 
