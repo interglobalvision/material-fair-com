5c1169c97cf1c4a0ce2ca12143c044785f5bc5e3
Michael Ray-Von <mikey@interglobal.vision>
2016-07-18 15:16:04 -0500

    fix fair year conditional

--- archive-exhibitor.php
+++ archive-exhibitor.php
@@ -6,6 +6,7 @@ $exhibitors_apply_text = IGV_get_option('_igv_page_options', '_igv_exhibitors_ap
 $apply_url = IGV_get_option('_igv_site_options', '_igv_apply_url');
 $apply_end = IGV_get_option('_igv_site_options', '_igv_apply_end');
 $current_year_id = IGV_get_option('_igv_site_options', '_igv_current_fair_year');
+$fair_year_id = get_fair_year_id();
 ?>
 
 <!-- main content -->
@@ -20,7 +21,7 @@ $current_year_id = IGV_get_option('_igv_site_options', '_igv_current_fair_year')
 // if the application hasn't ended and 
 // exhibitors are not published, 
 // show the Apply Now section
-if (($current_year_id == get_fair_year_id() && $publish_exhibitors == 'on') || $current_year_id != $fair_year_id) {
+if (($current_year_id == $fair_year_id && $publish_exhibitors == 'on') || $current_year_id != $fair_year_id) {
 
   get_template_part('partials/archive-exhibitor/exhibitors');
 
