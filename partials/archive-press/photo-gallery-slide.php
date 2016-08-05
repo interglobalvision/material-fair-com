336b18cff8d92f36a2ffc5fd8d7f40e688ae08e1
Michael Ray-Von <mikey@interglobal.vision>
2016-08-05 16:15:36 -0700

    add identifier class to slide caption

--- partials/archive-press/photo-gallery-slide.php
+++ partials/archive-press/photo-gallery-slide.php
@@ -11,7 +11,7 @@ if ( get_post_gallery($post->ID) ) {
 ?>
   <div class="swiper-slide flex-col justify-center align-center">
     <img src="<?php echo wp_get_attachment_image_src($gid, 'col-8')[0]; ?>">
-    <div class="margin-top-tiny text-align-center">
+    <div class="padding-top-tiny text-align-center slide-caption">
       <div class="font-size-h4"><?php _e($description); ?></div>
       <?php _e($caption); ?>
     </div>
