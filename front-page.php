4b6550185597be960d891f19d614c836342ec2be
Michael Ray-Von <mikey@interglobal.vision>
2016-08-05 18:24:08 -0700

    move front page program section display conditional into front-page template

--- front-page.php
+++ front-page.php
@@ -2,30 +2,35 @@
 get_header();
 
 $publish_program = IGV_get_option('_igv_page_options', '_igv_publish_program');
-$publish_exhibitors = IGV_get_option('_igv_page_options', '_igv_publish_exhibitors');
 ?>
 
 <!-- main content -->
 
 <main id="main-content">
 
-<?php get_template_part('partials/front-page/splash'); ?>
-
-<?php get_template_part('partials/front-page/introduction'); ?>
-
 <?php 
+
+get_template_part('partials/front-page/splash'); 
+
+get_template_part('partials/front-page/introduction'); 
+ 
 // FEATURED CONTENT SECTION
+
+get_template_part('partials/front-page/visitor-info'); 
+
+get_template_part('partials/front-page/exhibitors'); 
+
+if ( $publish_program == 'on' ) {
+
+  get_template_part('partials/archive-event/highlights');
+
+}
+
+get_template_part('partials/front-page/press'); 
+
+get_template_part('partials/partners'); 
+
 ?>
-
-<?php get_template_part('partials/front-page/visitor-info'); ?>
-
-<?php get_template_part('partials/front-page/exhibitors'); ?>
-
-<?php get_template_part('partials/front-page/program'); ?>
-
-<?php get_template_part('partials/front-page/press'); ?>
-
-<?php get_template_part('partials/partners'); ?>
 
 <!-- end main-content -->
 
--- partials/front-page/program.php
+++
@@ -1,9 +0,0 @@
-<?php
-$publish_program = IGV_get_option('_igv_page_options', '_igv_publish_program');
-
-if ( $publish_program == 'on' ) {
-
-  get_template_part('partials/archive-event/highlights');
-
-}
-?>
