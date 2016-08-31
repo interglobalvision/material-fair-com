56fcd0baa364955bc58f2bb0b4d6282a255180af
Michael Ray-Von <michaelray-von@Michaels-MacBook-Pro.local>
2016-08-31 18:11:56 -0500

    update index template

--- index.php
+++ index.php
@@ -2,41 +2,60 @@
 get_header();
 ?>
 
-<!-- main content -->
-
 <main id="main-content">
 
-  <!-- main posts loop -->
-  <section id="posts">
+<?php 
+$args = (
+  'posts_per_page' => 1,
+);
+
+$highlight = new WP_Query($args);
+if ($highlight->have_posts()) {
+  while ($highlight->have_posts()) {
+    the_post();
+?>
+  <section class="section">
+    <div class="container">
+      <div class="row">
+        <article <?php post_class('col col-s col-s-12 col-m col-l'); ?> id="post-<?php the_ID(); ?>">
+          Highlight
+        </article>
+      </div>
+    </div>
+  </section>
+<?php 
+  }
+  wp_reset_postdata();
+}
+?>
+
+  <section id="posts" class="section">
+    <div class="container">
+      <div class="row">
 
 <?php
 if( have_posts() ) {
   while( have_posts() ) {
     the_post();
 ?>
+      <article <?php post_class('col col-s col-s-12 col-m col-m-6 col-l margin-bottom-small'); ?> id="post-<?php the_ID(); ?>">
 
-    <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+        <?php get_template_part('index/feed-post'); ?>
 
-      <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
-
-      <?php the_content(); ?>
-
-    </article>
-
+      </article>
 <?php
   }
 } else {
 ?>
-    <article class="u-alert"><?php _e('Sorry, no posts matched your criteria :{'); ?></article>
+        <article class="u-alert"><?php _e('Sorry, no posts matched your criteria :{'); ?></article>
 <?php
 } ?>
 
-  <!-- end posts -->
+      </div>
+    </div>
   </section>
 
-  <?php get_template_part('partials/pagination'); ?>
-
-<!-- end main-content -->
+    <?php get_template_part('partials/pagination'); ?>
 
 </main>
 
