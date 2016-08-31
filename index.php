fb0d4cd2c212aeda5367a0b4b216e90c6f80d29f
Michael Ray-Von <michaelray-von@Michaels-MacBook-Pro.local>
2016-08-31 18:33:28 -0500

    show hightlight only on first page. fix template names

--- index.php
+++ index.php
@@ -5,30 +5,33 @@ get_header();
 <main id="main-content">
 
 <?php 
-$args = array(
-  'posts_per_page' => 1,
-);
+/*
+if (!is_paged()) {
+  $args = array(
+    'posts_per_page' => 1,
+  );
 
-$highlight = new WP_Query($args);
-if ($highlight->have_posts()) {
-  while ($highlight->have_posts()) {
-    the_post();
+  $highlight = new WP_Query($args);
+  if ($highlight->have_posts()) {
+    while ($highlight->have_posts()) {
+      the_post();
 ?>
   <section class="section">
     <div class="container">
       <div class="row">
         <article <?php post_class('col col-s col-s-12 col-m col-l'); ?> id="post-<?php the_ID(); ?>">
 
-          <?php get_template_part('index/highlight'); ?>
+          <?php get_template_part('partials/index/highlight'); ?>
 
         </article>
       </div>
     </div>
   </section>
 <?php 
+    }
+    wp_reset_postdata();
   }
-  wp_reset_postdata();
-}
+}*/
 ?>
 
   <section id="posts" class="section">
@@ -42,7 +45,7 @@ if( have_posts() ) {
 ?>
       <article <?php post_class('col col-s col-s-12 col-m col-m-6 col-l margin-bottom-small'); ?> id="post-<?php the_ID(); ?>">
 
-        <?php get_template_part('index/post'); ?>
+        <?php get_template_part('partials/index/post'); ?>
 
       </article>
 <?php
