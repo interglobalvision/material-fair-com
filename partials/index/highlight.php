d15654ef561a9a13837f34d8f4945bfd9ac0116d
Michael Ray-Von <michaelray-von@Michaels-MacBook-Pro.local>
2016-09-02 17:57:54 -0500

    add highlight markup

--- partials/index/highlight.php
+++ partials/index/highlight.php
@@ -1,4 +1,24 @@
-<a href="<?php the_permalink() ?>">
-  <?php the_post_thumbnail('col-6-crop'); ?>
-  <h2><?php the_title(); ?></h2>
-</a>
+<?php
+  $authors = get_the_terms($post->ID, 'post_author');
+  $cats = get_the_terms($post->ID, 'category');
+?>
+
+<div class="highlight-holder">
+
+  <a href="<?php the_permalink() ?>">
+    <?php the_post_thumbnail('col-12-crop', array('class'=>'highlight-image')); ?>
+  </a>
+
+  <div class="row highlight-details">
+    <div class="col-s col-s-10 col-m col-m-8 col-l section-yellow padding-top-small padding-bottom-small">
+      <div class="margin-bottom-micro">
+        <?php get_template_part('partials/index/post-details'); ?>
+      </div>
+
+      <a href="<?php the_permalink() ?>">
+        <h3 class="font-size-h1 margin-bottom-micro"><?php the_title(); ?></h3>
+      </a>
+    </div>
+  </div>
+
+</div>
