566266cc90e74403ca88ff4b0aa76d9685896942
Michael Ray-Von <michaelray-von@Michaels-MacBook-Pro.local>
2016-09-03 18:32:48 -0500

    move image above post details

--- partials/index/post.php
+++ partials/index/post.php
@@ -3,11 +3,11 @@
   $cats = get_the_terms($post->ID, 'category');
 ?>
 
+<a href="<?php the_permalink() ?>">
+  <?php the_post_thumbnail('col-6-crop'); ?>
+  <h3 class="margin-bottom-micro"><?php the_title(); ?></h3>
+</a>
+
 <div class="margin-bottom-micro">
   <?php get_template_part('partials/index/post-details'); ?>
 </div>
-
-<a href="<?php the_permalink() ?>">
-  <h3 class="margin-bottom-micro"><?php the_title(); ?></h3>
-  <?php the_post_thumbnail('col-6-crop'); ?>
-</a>
