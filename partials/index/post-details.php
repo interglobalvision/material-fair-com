0d6b9780c22f9a9eda4f579e010157eede3f106f
Michael Ray-Von <michaelray-von@Michaels-MacBook-Pro.local>
2016-09-03 18:33:08 -0500

    add conditionals for post authors and cats

--- partials/index/post-details.php
+++ partials/index/post-details.php
@@ -4,24 +4,28 @@
 
   $post_details = get_the_date('j M Y') . ' | ';
 
-  if (count($authors) > 1) {
-    $author_string = '';
-    foreach ($authors as $author) {
-      $author_string .= $author->name . ', ';
+  if ($authors) {
+    if (count($authors) > 1) {
+      $author_string = '';
+      foreach ($authors as $author) {
+        $author_string .= $author->name . ', ';
+      }
+      $post_details .= rtrim($author_string, ', ') . ' | ';
+    } else {
+      $post_details .= $authors[0]->name . ' | ';
     }
-    $post_details .= rtrim($author_string, ', ') . ' | ';
-  } else {
-    $post_details .= $authors[0]->name . ' | ';
   }
 
-  if (count($cats) > 1) {
-    $cat_string = '';
-    foreach ($cats as $cat) {
-      $cat_string .= '<a href="' . get_term_link($cat->term_id) . '">' . $cat->name . '</a>, ';
+  if ($cats && array_values($cats)[0]->slug !== 'uncategorized'){
+    if (count($cats) > 1) {
+      $cat_string = '';
+      foreach ($cats as $cat) {
+        $cat_string .= '<a href="' . get_term_link($cat->term_id) . '">' . $cat->name . '</a>, ';
+      }
+      $post_details .= rtrim($cat_string, ', ') . ' | ';
+    } else {
+      $post_details .= '<a href="' . get_term_link($cats[0]->term_id) . '">' . $cats[0]->name . '</a> | ';
     }
-    $post_details .= rtrim($cat_string, ', ') . ' | ';
-  } else {
-    $post_details .= '<a href="' . get_term_link($cats[0]->term_id) . '">' . $cats[0]->name . '</a> | ';
   }
 
   echo rtrim($post_details, ' | ');
