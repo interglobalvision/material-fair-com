8f3c64352bf03140bcf931b0c70a3b3f1a353ad2
Michael Ray-Von <michaelray-von@Michaels-MacBook-Pro.local>
2016-09-04 22:01:12 -0500

    add single post pagination

--- partials/index/pagination.php
+++ partials/index/pagination.php
@@ -35,22 +35,42 @@ if ($cats && array_values($cats)[0]->slug !== 'uncategorized'){
   </div>
 
 <?php
-if (get_next_posts_link() || get_previous_posts_link()) {
+if (is_single()) {
+  $show_previous = get_previous_post_link();
+  $show_next = get_next_post_link();
+} else {
+  $show_previous = get_previous_posts_link();
+  $show_next = get_next_posts_link();
+}
+
+if ($show_previous || $show_next) {
 ?>
   <div class="col col-s col-s-12 col-m col-m-4 col-l col-l-4 margin-bottom-tiny flex-row pagination-container align-start">
   <!-- post pagination -->
 <?php
-  if (get_previous_posts_link()) {
+  if ($show_previous) {
+    if (is_single()) {
+      $adjacent_post = get_adjacent_post(false, '', true);
+      $post_link = get_permalink($adjacent_post);
+    } else {
+      $post_link = previous_posts(false);
+    }
 ?>
-    <a class="button pagination-button flex-row align-center" href="<?php previous_posts(); ?>">
+    <a class="button pagination-button flex-row align-center" href="<?php echo $post_link; ?>">
       <div>◀&nbsp;</div><div><?php _e('[:es]Anterior[:en]Previous'); ?></div>
     </a>
 <?php
   }
 
-  if (get_next_posts_link()) {
+  if ($show_next) {
+    if (is_single()) {
+      $adjacent_post = get_adjacent_post(false, '', false);
+      $post_link = get_permalink($adjacent_post);
+    } else {
+      $post_link = next_posts(false);
+    }
 ?>
-    <a class="button pagination-button flex-row align-center" href="<?php next_posts(); ?>">
+    <a class="button pagination-button flex-row align-center" href="<?php echo $post_link; ?>">
       <div><?php _e('[:es]Siguiente[:en]Next'); ?></div><div>&nbsp;▶</div>
     </a>
 <?php
