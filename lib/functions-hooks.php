b00e6da1306855c4c729e8901c4cf278104f3a22
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2016-11-28 18:03:41 -0600

    clean up and comment add_year_booth_levels hook

--- lib/functions-hooks.php
+++ lib/functions-hooks.php
@@ -2,26 +2,26 @@
 
 // Custom hooks (like excerpt length etc)
 
+// add Exhibitor and Project booth level terms for each new fair year term
 function add_year_booth_levels( $term_id, $tt_id, $taxonomy ){
+  // only run this on new fair year terms
   if ($taxonomy === 'fair_year') {
     $year_term = get_term($term_id, $taxonomy);
+    $year_slug = $year_term->slug;
 
-    if ($year_term->parent === 0) {
-      $year_slug = $year_term->slug;
-      $terms = array('Exhibitor', 'Project');
+    // array of booth level terms to create
+    $terms = array('Exhibitor', 'Project');
 
-      foreach ($terms as $term) {
-        wp_insert_term( 
-          $year_slug . ' ' . $term, 
-          'booth_level', 
-          array(
-            'slug'    => $year_slug . '-' . strtolower($term)
-          ) 
-        );
-      }
+    foreach ($terms as $term) {
+      // insert booth level term
+      wp_insert_term( 
+        $year_slug . ' ' . $term, 
+        'booth_level', 
+        array(
+          'slug'    => $year_slug . '-' . strtolower($term)
+        ) 
+      );
     }
-
-    die;
   }
 }
 add_action( 'create_term', 'add_year_booth_levels', 10, 3 );
