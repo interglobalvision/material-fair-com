6706798b748cfcf3c08246c70f98e5168f9742f7
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2016-12-13 17:47:04 -0600

    add rsvp form

--- page-vip.php
+++ page-vip.php
@@ -19,25 +19,46 @@ if( have_posts() ) {
     <div class="container">
       <div class="row">
         <div class="col col-s col-s-12 col-m col-m-12 col-l col-l-12">
-          <h1><?php the_title(); ?></h1>
+          <h1><?php _e('[:en]VIP Program[:es]Programa VIP'); ?></h1>
         </div>
       </div>
       <div class="row">
 <?php 
     if (has_post_thumbnail() && !post_password_required()) {
 ?>
-    <div class="<?php echo $colClasses; ?>">
-      <?php the_post_thumbnail(); ?>
-    </div>
+        <div class="<?php echo $colClasses; ?>">
+          <?php the_post_thumbnail(); ?>
+        </div>
 <?php
     }
 ?>
-    <div class="<?php echo $colClasses; ?>">
-      <div class="font-size-h3">
-        <?php the_content(); ?>
+        <div class="<?php echo $colClasses; ?>">
+          <div class="font-size-h3">
+            <?php the_content(); ?>
+          </div>
+        </div>
       </div>
+<?php 
+    if (!post_password_required()) {
+      $rsvp_text = get_post_meta($post->ID, '_igv_vip_rsvp_text', true);
+?>
     </div>
-
+  </section>
+  <section id="vip-rsvp" class="section">
+    <div class="container">
+      <div class="row">
+        <div id="vip-rsvp-text-container" class="col col-s col-s-12 col-m col-m-12 col-l col-l-6 font-size-h1 font-sans">
+          <?php echo !empty($rsvp_text) ? apply_filters('the_content', $rsvp_text) : ''; ?>
+          <button class="button font-size-h3 margin-top-small margin-bottom-small" id="show-vip-rsvp"><?php _e('[:en]I\'m Attending[:es]Asistiré'); ?></button>
+        </div>
+        <div id="vip-rsvp-form-container" class="col col-s col-s-12 col-m col-m-12 col-l col-l-6 offset-l-6">
+          <h2 class="margin-bottom-small">RSVP</h2>
+          <?php gravity_form('VIP RSVP', false, false, false, '', true); ?>
+        </div>
+      </div>
+<?php 
+    } 
+?>
     </div>
   </section>
 
