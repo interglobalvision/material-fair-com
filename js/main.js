7b4b6482fccfa37d87060658c2bed3c5703f86a7
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2016-12-15 15:20:58 -0600

    remove form language js

--- js/main.js
+++ js/main.js
@@ -221,17 +221,8 @@ Site.Vip = {
     $('#show-vip-rsvp').on('click', function() {
       $('#vip-rsvp-text-container').hide(200);
       $('#vip-rsvp-form-container').show(200);
-      _this.fillFormLang();
     })
   },
-
-  fillFormLang: function() {
-    if (currentLang === 'es') {
-      $('ul.gform_fields li:last-child input').val('Español');
-    } else {
-      $('ul.gform_fields li:last-child input').val('English');
-    }
-  }
 }
 
 jQuery(document).ready(function () {
