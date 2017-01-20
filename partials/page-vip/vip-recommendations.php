4d09ea1cd69b9fe152f1223bf887d013dc28ec3d
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-01-20 15:22:44 -0600

    fix vip recommendations column layout

--- partials/page-vip/vip-recommendations.php
+++ partials/page-vip/vip-recommendations.php
@@ -8,38 +8,46 @@ if (!empty($museums) || !empty($galleries) || !empty($restaurants) || !empty($ot
 ?>
 <section id="vip-recommendations" class="section section-black">
   <div class="container">
-    <div class="row">
+
 <?php
   if (!empty($museums)) {
 ?>
-      <div class="col col-s col-s-12 col-m col-m-6 col-l col-l-6">
+    <div class="row">
+      <div class="col col-s col-s-12 col-m col-m-12 col-l col-l-12">
         <h2 class="margin-bottom-tiny"><?php _e('[:en]Museums[:es]Museos[:]'); ?></h2>
         <?php echo apply_filters('the_content', $museums); ?>
       </div>
+    </div>
 <?php
   }
   if (!empty($galleries)) {
 ?>
-      <div class="col col-s col-s-12 col-m col-m-6 col-l col-l-6">
+    <div class="row">
+      <div class="col col-s col-s-12 col-m col-m-12 col-l col-l-12">
         <h2 class="margin-bottom-tiny"><?php _e('[:en]Galleries & Project Spaces[:es]Galerías y Espacios de Proyectos[:]'); ?></h2>
         <?php echo apply_filters('the_content', $galleries); ?>
       </div>
+    </div>
 <?php
   }
   if (!empty($restaurants)) {
 ?>
-      <div class="col col-s col-s-12 col-m col-m-6 col-l col-l-6">
+    <div class="row">
+      <div class="col col-s col-s-12 col-m col-m-12 col-l col-l-12">
         <h2 class="margin-bottom-tiny"><?php _e('[:en]Restaurants & Bars[:es]Restaurantes y Bares[:]'); ?></h2>
         <?php echo apply_filters('the_content', $restaurants); ?>
       </div>
+    </div>
 <?php
   }
   if (!empty($other)) {
 ?>
-      <div class="col col-s col-s-12 col-m col-m-6 col-l col-l-6">
+    <div class="row">
+      <div class="col col-s col-s-12 col-m col-m-12 col-l col-l-12">
         <h2 class="margin-bottom-tiny"><?php _e('[:en]Other[:es]Otros[:]'); ?></h2>
         <?php echo apply_filters('the_content', $other); ?>
       </div>
+    </div>
 <?php
   }
 ?>
