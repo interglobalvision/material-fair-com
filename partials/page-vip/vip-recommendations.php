c96cc7d0f20c2cded832fc45f7eab986e04c99bc
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-01-20 15:08:55 -0600

    add columns for ul in vip recommendations

--- partials/page-vip/vip-recommendations.php
+++ partials/page-vip/vip-recommendations.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $museums = get_post_meta($post->ID, '_igv_vip_museums', true);
 $galleries = get_post_meta($post->ID, '_igv_vip_galleries', true);
 $restaurants = get_post_meta($post->ID, '_igv_vip_restaurants', true);
@@ -6,46 +6,46 @@ $other = get_post_meta($post->ID, '_igv_vip_other', true);
 
 if (!empty($museums) || !empty($galleries) || !empty($restaurants) || !empty($other)) {
 ?>
-<section id="vip-hotels" class="section section-black">
+<section id="vip-recommendations" class="section section-black">
   <div class="container">
     <div class="row">
 <?php
   if (!empty($museums)) {
 ?>
-      <div class="col col-s col-s-12 col-m col-m-6 col-l col-l-6"> 
+      <div class="col col-s col-s-12 col-m col-m-6 col-l col-l-6">
         <h2 class="margin-bottom-tiny"><?php _e('[:en]Museums[:es]Museos[:]'); ?></h2>
         <?php echo apply_filters('the_content', $museums); ?>
       </div>
-<?php 
+<?php
   }
   if (!empty($galleries)) {
 ?>
-      <div class="col col-s col-s-12 col-m col-m-6 col-l col-l-6"> 
+      <div class="col col-s col-s-12 col-m col-m-6 col-l col-l-6">
         <h2 class="margin-bottom-tiny"><?php _e('[:en]Galleries & Project Spaces[:es]Galerías y Espacios de Proyectos[:]'); ?></h2>
         <?php echo apply_filters('the_content', $galleries); ?>
       </div>
-<?php 
+<?php
   }
   if (!empty($restaurants)) {
 ?>
-      <div class="col col-s col-s-12 col-m col-m-6 col-l col-l-6"> 
+      <div class="col col-s col-s-12 col-m col-m-6 col-l col-l-6">
         <h2 class="margin-bottom-tiny"><?php _e('[:en]Restaurants & Bars[:es]Restaurantes y Bares[:]'); ?></h2>
         <?php echo apply_filters('the_content', $restaurants); ?>
       </div>
-<?php 
+<?php
   }
   if (!empty($other)) {
 ?>
-      <div class="col col-s col-s-12 col-m col-m-6 col-l col-l-6"> 
+      <div class="col col-s col-s-12 col-m col-m-6 col-l col-l-6">
         <h2 class="margin-bottom-tiny"><?php _e('[:en]Other[:es]Otros[:]'); ?></h2>
         <?php echo apply_filters('the_content', $other); ?>
       </div>
-<?php 
+<?php
   }
 ?>
     </div>
   </div>
 </section>
-<?php 
+<?php
 }
 ?>
